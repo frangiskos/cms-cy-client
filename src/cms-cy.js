64e18f8a6c6a7ee4e58bf99a3f2323dea084b1af
Frangiskos <frangiskos@gmail.com>
2023-01-13 00:37:52 +0200

    feat: add paging

--- src/cms-cy.js
+++ src/cms-cy.js
@@ -24,6 +24,7 @@
                 offset
                 order_by
                 order
+                has_paging
                 image_width
                 aspect_ratio
                 fit
@@ -61,10 +62,12 @@
             $sortField: [String] = ["-date_published"]
             $offset: Int = 0
             $limit: Int = 100
+            $page: Int = 1
             $isFeatureFilterOperator: boolean_filter_operators = { _nnull: true }
         ) {
             # Get all articles
             Articles(
+                page: $page
                 filter: {
                     _and: [
                         { is_feature: $isFeatureFilterOperator }
@@ -97,6 +100,23 @@
                         title
                         slug
                     }
+                }
+            }
+            Articles_aggregated(
+                filter: {
+                    _and: [
+                        { is_feature: $isFeatureFilterOperator }
+                        {
+                            _or: [
+                                { article_categories: { ArticleCategories_id: { slug: { _in: $categories } } } }
+                                { id: { _in: $articleIds } }
+                            ]
+                        }
+                    ]
+                }
+            ) {
+                count {
+                    id
                 }
             }
         }
@@ -197,16 +217,16 @@
         const data = await fetchData(query, {
             variables,
         });
-        return data.data[Object.keys(data.data)[0]];
+        return Object.keys(data.data).map((k) => data.data[k]);
     };
 
     const loadArticleList = async (name) => {
         const articleListComponent = await getData(ArticleListComponentQuery, { component: name });
-        if (!articleListComponent || !articleListComponent.length) {
+        if (!articleListComponent || !articleListComponent.length || !articleListComponent[0].length) {
             console.error(`CMS Error: component "${name}" is not configured`);
             return '';
         }
-        const component = articleListComponent[0];
+        const component = articleListComponent[0][0];
 
         const categories = [];
         const articles = [];
@@ -224,9 +244,9 @@
                 const articleSlug = urlParams.get('article');
                 if (articleSlug) {
                     const articleCategories = await getData(ArticleCategoriesQuery, { slug: articleSlug });
-                    if (articleCategories && articleCategories.length) {
+                    if (articleCategories && articleCategories.length && articleCategories[0].length) {
                         categories.push(
-                            ...articleCategories[0].article_categories.map((cat) => cat.ArticleCategories_id.slug)
+                            ...articleCategories[0][0].article_categories.map((cat) => cat.ArticleCategories_id.slug)
                         );
                     }
                 }
@@ -252,12 +272,22 @@
             articles.push(...component.include_articles.map((article) => +article.Articles_id.id));
         }
 
+        let currentPage = 1;
+        if (component.has_paging) {
+            const urlParams = new URLSearchParams(window.location.search);
+            const pageParam = urlParams.get('page');
+            if (pageParam && !isNaN(+pageParam)) {
+                currentPage = +pageParam;
+            }
+        }
+
         const articleList = await getData(ArticleListQuery, {
             categories: categories.length ? categories : [''],
             articleIds: articles.length ? articles : [-1],
             sortField: (component.order === 'desc' ? '-' : '') + component.order_by,
             offset: component.offset,
             limit: component.columns * component.rows,
+            page: currentPage,
             isFeatureFilterOperator:
                 component.featured === 'all'
                     ? { _nnull: true }
@@ -265,9 +295,44 @@
                     ? { _eq: true }
                     : { _eq: false },
         });
-        if (!articleList || !articleList.length) {
+        if (!articleList || !articleList.length || !articleList[0].length) {
             console.warn(`CMS Warning: component "${name}" does not have any articles`);
             return '';
+        }
+
+        let previousPageUrl = '';
+        let nextPageUrl = '';
+        let totalPages = 1;
+        const pages = [];
+        if (component.has_paging) {
+            const urlParams = new URLSearchParams(window.location.search);
+            urlParams.delete('page');
+            const baseUrl = window.location.href.replace
+                ? window.location.href.replace(window.location.search, '')
+                : window.location.href.split('?')[0];
+
+            totalPages = Math.ceil(articleList[1][0].count.id / (component.columns * component.rows));
+            for (let i = 1; i <= totalPages; i++) {
+                const url =
+                    i === 1
+                        ? urlParams.toString()
+                            ? `${baseUrl}?${urlParams.toString()}`
+                            : baseUrl
+                        : urlParams.toString()
+                        ? `${baseUrl}?${urlParams.toString()}&page=${i}`
+                        : `${baseUrl}?page=${i}`;
+                pages.push({
+                    text: i,
+                    url,
+                    active: i === currentPage,
+                });
+                if (i === currentPage - 1) {
+                    previousPageUrl = url;
+                }
+                if (i === currentPage + 1) {
+                    nextPageUrl = url;
+                }
+            }
         }
 
         const totalHGap = component.spacing * (component.columns - 1);
@@ -284,7 +349,7 @@ width: calc(100% / ${component.columns} - ${itemGapSpace}px);
 ${component.custom_code ? component.custom_code : ''}
 
 <div id="${name}" class="cms-cy-posts__wrapper">
-${articleList
+${articleList[0]
     .map(
         (item) => `
 <div class="cms-cy-posts__item-wrapper">
@@ -316,6 +381,30 @@ ${articleList
 `
     )
     .join('')}
+    ${
+        component.has_paging && pages.length > 1
+            ? `<div class="cms-cy-posts__paging">
+            <a ${previousPageUrl ? `href="${previousPageUrl}" class="previous"` : 'class="previous disabled"'}>
+                <span class="sr-only">Previous</span>
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
+                <path fill-rule="evenodd" d="M12.79 5.23a.75.75 0 01-.02 1.06L8.832 10l3.938 3.71a.75.75 0 11-1.04 1.08l-4.5-4.25a.75.75 0 010-1.08l4.5-4.25a.75.75 0 011.06.02z" clip-rule="evenodd" />
+              </svg>
+                </a>
+        ${pages
+            .map(
+                (page) =>
+                    `<a href="${page.url}" ${page.active ? 'class="page active"' : 'class="page"'}>${page.text}</a>`
+            )
+            .join('')}
+         <a ${nextPageUrl ? `href="${nextPageUrl}" class="next"` : 'class="next disabled"'}>
+            <span class="sr-only">Next</span>
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
+            <path fill-rule="evenodd" d="M7.21 14.77a.75.75 0 01.02-1.06L11.168 10 7.23 6.29a.75.75 0 111.04-1.08l4.5 4.25a.75.75 0 010 1.08l-4.5 4.25a.75.75 0 01-1.06-.02z" clip-rule="evenodd" />
+            </svg>
+         </a>
+    </div>`
+            : ''
+    }
 </div>
 `;
 
@@ -627,6 +716,75 @@ ${articleList
                         margin-bottom: 24px;
                     }
                 }
+
+                .cms-cy-posts__paging {
+                    margin-top: 40px;
+                    box-shadow: 0 4px 12px rgb(100 100 100 / 60%);
+                    border-radius: 500px;
+                    width: fit-content !important;
+                }
+
+                .cms-cy-posts__paging a {
+                    color: inherit;
+                    border: 1px solid;
+                    min-width: 40px;
+                    text-align: center;
+                    line-height: 40px;
+                    min-height: 40px;
+                    margin: 0;
+                    padding: 0;
+                }
+
+                .cms-cy-posts__paging a:visited {
+                    color: inherit;
+                }
+
+                .cms-cy-posts__paging a.disabled {
+                    opacity: 0.5;
+                    cursor: not-allowed;
+                }
+                .cms-cy-posts__paging a.active {
+                    border: 2px solid;
+                }
+                .cms-cy-posts__paging a.previous,
+                .cms-cy-posts__paging a.next {
+                    min-width: 60px;
+                    text-align: center;
+                    display: flex;
+                    align-items: center;
+                    justify-content: center;
+                }
+                .cms-cy-posts__paging a.previous svg,
+                .cms-cy-posts__paging a.next svg {
+                    width: 32px;
+                    height: 32px;
+                }
+                .cms-cy-posts__paging a.previous {
+                    border-radius: 500px 0 0 500px;
+                }
+                .cms-cy-posts__paging a.next {
+                    border-radius: 0 500px 500px 0;
+                }
+                .sr-only {
+                    position: absolute;
+                    width: 1px;
+                    height: 1px;
+                    padding: 0;
+                    margin: -1px;
+                    overflow: hidden;
+                    clip: rect(0, 0, 0, 0);
+                    white-space: nowrap;
+                    border-width: 0;
+                }
+                @media (max-width: 767px) {
+                    .cms-cy-posts__paging a.previous,
+                    .cms-cy-posts__paging a.next {
+                        min-width: 80px;
+                    }
+                    .cms-cy-posts__paging a.page {
+                        display: none;
+                    }
+                }
             `;
 
             // `
@@ -654,6 +812,9 @@ ${articleList
                 }
                 case 'single-article': {
                     loadArticle(name).then((template) => {
+                        // const articleElement = document.createElement('article');
+                        // articleElement.innerHTML = template;
+                        // element.parentNode.replaceChild(articleElement, element);
                         element.innerHTML = template;
                     });
                     break;
